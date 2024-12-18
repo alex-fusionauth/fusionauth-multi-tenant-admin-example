8603549274988cff02a1411558651e21c365e203
Alex Patterson <alex@codingcat.dev>
2024-12-18 12:34:46 -0500

    fix hydration issue

--- components/login-avatar.tsx
+++ components/login-avatar.tsx
@@ -9,30 +9,28 @@ import {
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { LogOut, User } from 'lucide-react';
 import { useFusionAuth } from '@fusionauth/react-sdk';
+import { useState, useEffect } from 'react';
 
 export default function LoginAvatar() {
-  const { isLoggedIn, startLogout } = useFusionAuth()
+  const { startLogout } = useFusionAuth();
+
   return (
-    <>
-      {isLoggedIn && (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-              <Avatar className="h-8 w-8">
-                <AvatarFallback>
-                  <User className="h-4 w-4" />
-                </AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem className='cursor-pointer' onClick={() => startLogout()}>
-              <LogOut className="mr-2 h-4 w-4" />
-              Log Out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarFallback>
+              <User className="h-4 w-4" />
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem className='cursor-pointer' onClick={() => startLogout()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
