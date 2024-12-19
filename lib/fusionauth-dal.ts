98ad0a09849ca03b2463336237d02f33497f96a6
Alex Patterson <alex@codingcat.dev>
2024-12-19 15:13:59 -0500

    add types

--- lib/fusionauth-dal.ts
+++ lib/fusionauth-dal.ts
@@ -6,10 +6,25 @@ import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import { JWTPayload } from 'jose';
 
-import { FusionAuthClient, UUID } from '@fusionauth/typescript-client';
+import { FusionAuthClient, SearchRequest, UUID } from '@fusionauth/typescript-client';
 
 export interface SessionPayload extends JWTPayload {
     roles?: string[];
+}
+
+//TODO: Maybe we should use fetch to take advantage of caching https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#caching-data-with-an-orm-or-database
+
+export enum Service {
+    Tenant = 'tenant',
+    Application = 'application',
+    User = 'user',
+}
+
+export enum Action {
+    View = 'view',
+    Create = 'create',
+    Update = 'update',
+    Delete = 'delete',
 }
 
 class FusionAuthClientWithSession extends FusionAuthClient {
@@ -24,7 +39,7 @@ class FusionAuthClientWithSession extends FusionAuthClient {
     //TODO: Do we care about sessions on API calls??
 
     private async ensureSession() {
-        // if (this.session) return; // Session already verified
+        // if (this.session) return; // Session already verified **removed as it is being cached in memory**
         try {
 
             const app_at = (await cookies()).get('app.at')?.value;
@@ -42,7 +57,7 @@ class FusionAuthClientWithSession extends FusionAuthClient {
         }
     }
 
-    private roleValidation(service: string, resource: string | UUID | null | undefined, action: string): boolean {
+    private roleValidation(service: Service, resource: string | UUID | null | undefined, action: Action): boolean {
         if (resource === null || resource === undefined) return false;
 
         if (!this.session || !this.session.roles) return false;
@@ -61,40 +76,65 @@ class FusionAuthClientWithSession extends FusionAuthClient {
 
 
     // Tenants
-
     public async retrieveTenants() {
         await this.ensureSession();
 
         const cached = await cache(async () => {
-            console.log('cache miss')
             const resp = await super.retrieveTenants();
-            const tenants = resp?.response?.tenants?.filter(tenant => this.roleValidation('tenant', tenant?.id, 'view'));
-            resp.response.tenants = tenants;
+            resp.response.tenants = resp?.response?.tenants?.filter(f => this.roleValidation(Service.Tenant, f?.id, Action.View));
             return resp;
         })();
         return cached;
     }
 
-    private cachedRetrieveTenant = cache(super.retrieveTenant.bind(this)); // Correct binding
-    public async retrieveTenant(tenantId: string) {
+    public async retrieveTenant(tenantId: UUID) {
         await this.ensureSession();
-        return this.cachedRetrieveTenant(tenantId);
+
+        const cached = await cache(async () => {
+            const resp = await super.retrieveTenant(tenantId);
+            resp.response.tenant = this.roleValidation(Service.Tenant, resp?.response?.tenant?.id, Action.View) ?
+                resp.response.tenant : undefined;
+            return resp;
+        })();
+        return cached;
     }
 
     // Applications
-    private cachedRetrieveApplications = cache(super.retrieveApplications.bind(this));
     public async retrieveApplications() {
         await this.ensureSession();
-        return this.cachedRetrieveApplications();
+
+        const cached = await cache(async () => {
+            const resp = await super.retrieveApplications();
+            resp.response.applications = resp?.response?.applications?.filter(f => this.roleValidation(Service.Application, f?.id, Action.View));
+            return resp;
+        })();
+        return cached;
     }
 
-    private cachedRetrieveApplication = cache(super.retrieveApplication.bind(this));
-    public async retrieveApplication(applicationId: string) {
+    public async retrieveApplication(applicationId: UUID) {
         await this.ensureSession();
-        return this.cachedRetrieveApplication(applicationId);
+
+        const cached = await cache(async () => {
+            const resp = await super.retrieveApplication(applicationId);
+            resp.response.application = this.roleValidation(Service.Application, resp?.response?.application?.id, Action.View) ?
+                resp.response.application : undefined;
+            return resp;
+        })();
+        return cached;
     }
 
     // Users
+    public async searchUsersByQuery(request: SearchRequest) {
+        await this.ensureSession();
+
+        const cached = await cache(async () => {
+            const resp = await super.searchUsersByQuery(request);
+            resp.response.users = resp?.response?.users?.filter(f => this.roleValidation(Service.User, f?.id, Action.View));
+            return resp;
+        })();
+        return cached;
+    }
+
     private cachedRetrieveUser = cache(super.retrieveUser.bind(this));
     public async retrieveUser(userId: UUID) {
         await this.ensureSession();
